28334bb003991af3616a0042816652b9656d40fb
Sean G. Wright <sean@seangwright.me>
2024-01-23 17:52:41 -0500

    fix(DancingGoat): MSBuild analzyer warnings/errors

--- examples/DancingGoat/Models/Reusable/Cafe/CafeViewModel.cs
+++ examples/DancingGoat/Models/Reusable/Cafe/CafeViewModel.cs
@@ -1,12 +1,7 @@
-﻿using System.Linq;
-
-namespace DancingGoat.Models
+﻿namespace DancingGoat.Models
 {
     public record CafeViewModel(string Name, string PhotoPath, string PhotoShortDescription, string Street, string City, string Country, string ZipCode, string Phone)
     {
-        /// <summary>
-        /// Maps <see cref=Cafe"/> to a <see cref="CafeViewModel"/>.
-        /// </summary>
         public static CafeViewModel GetViewModel(Cafe cafe)
         {
             var cafePhoto = cafe.CafePhoto?.FirstOrDefault();
