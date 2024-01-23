d8a75a09e34043fbab7348e8ec1756accfa8adfd
Milan Šůstek <milan.sustek@bluesoft.cz>
2024-01-23 16:06:09 +0100

    module init fix

--- src/Kentico.Xperience.TagManager/Modules/CustomChannelSettingsModuleInstaller.cs
+++ src/Kentico.Xperience.TagManager/Modules/CustomChannelSettingsModuleInstaller.cs
@@ -45,7 +45,7 @@ internal class CustomChannelSettingsModuleInstaller : ICustomChannelSettingsModu
 
     private static void InstallChannelCodeSnippetClass(ResourceInfo resourceInfo)
     {
-        var channelCodeSnippetClass = DataClassInfoProvider.GetDataClassInfo(ChannelCodeSnippetInfo.OBJECT_TYPE) ??
+        var channelCodeSnippetClass = DataClassInfoProvider.GetDataClassInfo(ChannelCodeSnippetInfo.TYPEINFO.ObjectClassName) ??
                                       DataClassInfo.New(ChannelCodeSnippetInfo.OBJECT_TYPE);
 
         channelCodeSnippetClass.ClassName = ChannelCodeSnippetInfo.TYPEINFO.ObjectClassName;
