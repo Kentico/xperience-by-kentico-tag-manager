1df0721182ae3809f06b3aa9bc74d89cd1c4e2b9
bkapustik <82807109+bkapustik@users.noreply.github.com>
2024-05-14 15:06:07 +0200

    Update src/Kentico.Xperience.TagManager/Admin/CodeSnippetConfigurationModel.cs
    Co-authored-by: Sean G. Wright <seanw@kentico.com>

--- src/Kentico.Xperience.TagManager/Admin/CodeSnippetConfigurationModel.cs
+++ src/Kentico.Xperience.TagManager/Admin/CodeSnippetConfigurationModel.cs
@@ -62,7 +62,7 @@ internal static class CodeSnippetExtensions
                                                $"{nameof(CodeSnippetLocations.BodyBottom)};Insert at the bottom of the body\r\n";
 
     public const string DisplayModeFormComponentOptions = $"{nameof(CodeSnippetAdministrationDisplayMode.None)};Do not display in Administration\r\n" +
-                                                $"{nameof(CodeSnippetAdministrationDisplayMode.PreviewOnly)};Display in the Preview only\r\n" +
+                                                $"{nameof(CodeSnippetAdministrationDisplayMode.PreviewOnly)};Display in the Preview view mode only\r\n" +
                                                 $"{nameof(CodeSnippetAdministrationDisplayMode.PageBuilderOnly)};Display in the PageBuilder only\r\n" +
                                                 $"{nameof(CodeSnippetAdministrationDisplayMode.Both)};Display in Preview and Pagebuilder";
 }
