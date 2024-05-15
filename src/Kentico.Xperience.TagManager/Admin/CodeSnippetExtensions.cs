da03f931fedde785a1f1b7ffc54e6300fd386ca0
bkapustik <82807109+bkapustik@users.noreply.github.com>
2024-05-15 15:51:29 +0200

    fix(TagManager): CodeSnippetExtensions administration labels

--- src/Kentico.Xperience.TagManager/Admin/CodeSnippetExtensions.cs
+++ src/Kentico.Xperience.TagManager/Admin/CodeSnippetExtensions.cs
@@ -9,8 +9,8 @@ internal static class CodeSnippetExtensions
                                                $"{nameof(CodeSnippetLocations.BodyTop)};Insert at the top of the body\r\n" +
                                                $"{nameof(CodeSnippetLocations.BodyBottom)};Insert at the bottom of the body\r\n";
 
-    public const string DisplayModeFormComponentOptions = $"{nameof(CodeSnippetAdministrationDisplayMode.None)};Do not dispaly in Administration\r\n" +
-                                                $"{nameof(CodeSnippetAdministrationDisplayMode.PreviewOnly)};Display in the Preview only\r\n" +
-                                                $"{nameof(CodeSnippetAdministrationDisplayMode.PageBuilderOnly)};Display in the PageBuilder only\r\n" +
-                                                $"{nameof(CodeSnippetAdministrationDisplayMode.Both)};Display in Preview and Pagebuilder";
+    public const string DisplayModeFormComponentOptions = $"{nameof(CodeSnippetAdministrationDisplayMode.None)};Do not display in Administration\r\n" +
+                                                $"{nameof(CodeSnippetAdministrationDisplayMode.PreviewOnly)};Display in the Preview view mode only\r\n" +
+                                                $"{nameof(CodeSnippetAdministrationDisplayMode.PageBuilderOnly)};Display in the Page Builder only\r\n" +
+                                                $"{nameof(CodeSnippetAdministrationDisplayMode.Both)}Display in the Preview view mode and the Page Builder";
 }
